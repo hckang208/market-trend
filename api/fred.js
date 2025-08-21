79a450734fce0b9651f6d0ab47a7e432dae7a94a
hckang208 <hckang.hansoll@gmail.com>
2025-08-21 18:37:00 +0900

    Add files via upload

--- api/fred.js
+++ api/fred.js
@@ -3,7 +3,7 @@ import fetch from "node-fetch";
 export default async function handler(req, res) {
   try {
     const apiKey = process.env.FRED_API_KEY;
-    const seriesId = "GDP"; // 테스트용
+    const seriesId = "GDP";
     const url = `https://api.stlouisfed.org/fred/series/observations?series_id=${seriesId}&api_key=${apiKey}&file_type=json`;
 
     const response = await fetch(url);
