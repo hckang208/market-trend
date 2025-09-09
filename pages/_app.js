bfdf258c7430c6f1cf234ee037d753430be12717
hckang208 <hckang.hansoll@gmail.com>
2025-09-09 14:44:43 +0900

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,2 @@
-import Head from "next/head";
 import "@/styles/globals.css";
-
-export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <link href="https://cdn.jsdelivr.net/gh/sunn-us/SUIT/fonts/variable/woff2/SUIT-Variable.css" rel="stylesheet" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
-}
+export default function App({ Component, pageProps }) { return <Component {...pageProps} />; }
